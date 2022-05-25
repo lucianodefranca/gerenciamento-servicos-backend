13e80c33509a8c47052a4932b47c6ad3f197ed38
luciano de franca <lucianodefranca86@gmail.com>
2022-05-24 23:30:37 -0300

    Projeto iniciado

---
+++ src/main/java/com/luciano/gerenciamentoservicos/GerenciamentoServicosApplication.java
@@ -0,0 +1,13 @@
+package com.luciano.gerenciamentoservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoServicosApplication.class, args);
+	}
+
+}
