d0ae79c3aba9704ead1916e73b4e0de9416baaf9
luciano de franca <lucianodefranca86@gmail.com>
2022-05-29 22:32:16 -0300

    Excecao personalizada para objeto nao encontrado

--- src/main/java/com/luciano/gerenciamentoservicos/service/TecnicoService.java
+++ src/main/java/com/luciano/gerenciamentoservicos/service/TecnicoService.java
@@ -2,6 +2,7 @@ package com.luciano.gerenciamentoservicos.service;
 
 import com.luciano.gerenciamentoservicos.domain.Tecnico;
 import com.luciano.gerenciamentoservicos.repositories.TecnicoRepository;
+import com.luciano.gerenciamentoservicos.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID " + id));
     }
 
 }
